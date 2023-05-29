22a0fa8a364852ce1ec47d231a4e2396dee023bc
talha-amir165 <127726705+talha-amir165@users.noreply.github.com>
2023-05-29 12:45:02 +0500

    All Changes till date

--- index.js
+++ index.js
@@ -2,8 +2,32 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { Provider } from 'react-redux';
+import store from './app/store'
 
-AppRegistry.registerComponent(appName, () => App);
+import { name as appName } from './app.json';
+import PushNotification from "react-native-push-notification";
+PushNotification.configure({
+    // (optional) Called when Token is generated (iOS and Android)
+    onNotification: function(notification) {
+        console.log("NOTIFICATION:", notification);
+
+        // process the notification
+
+        // (required) Called when a remote is received or opened, or local notification is opened
+        notification.finish(PushNotificationIOS.FetchResult.NoData);
+    },
+
+});
+
+const AppWithRedux = () => ( <
+    Provider store = { store } >
+    <
+    App / >
+    <
+    /Provider>
+);
+
+AppRegistry.registerComponent(appName, () => AppWithRedux);
